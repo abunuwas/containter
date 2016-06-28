168f0a7a73bc4d78842992d0421028e85cf8711a
abunuwas <joseharoperalta@gmail.com>
2016-06-28 14:32:58 +0000

    Add app server

---
+++ server.py
@@ -0,0 +1,25 @@
+from app import app
+
+import cherrypy
+
+if __name__ == '__main__':
+    # Mount the application
+    cherrypy.tree.graft(app, "/")
+
+    # Unsubscrib e the default server
+    cherrypy.server.unsubscribe()
+
+    # Instantiate a new server object
+    server = cherrypy._cpserver.Server()
+
+    # Configure the server object
+    server.socket_host = "0.0.0.0"
+    server.socket_port = 80
+    server.thread_pool = 30
+
+    # Subscribe this server
+    server.subscribe()
+    
+    # Start the server engine
+    cherrypy.engine.start()
+    cherrypy.engine.block()
