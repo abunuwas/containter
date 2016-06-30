ab95709cf912d76da98213e681bb4c97070cc34d
abunuwas <joseharoperalta@gmail.com>
2016-06-30 09:13:45 +0100

    Add more features to app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hellow world!"
+    return "Hellow world! This is a new version :D :D :D :D"
+
+@app.rout("/new")
+def new():
+    return "This page is brand new!!!!!"
 
 if __name__ == "__main__":
     app.run()
