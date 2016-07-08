dea917200900b1b58120eaddf6e68363bee90f3c
abunuwas <joseharoperalta@gmail.com>
2016-07-08 16:39:18 +0100

    Add display information for debugging

--- application/src/app.py
+++ application/src/app.py
@@ -1,18 +1,26 @@
 from flask import Flask
 from redis import Redis
+from os.path import expanduser 
 
 app = Flask(__name__)
 redis = Redis(host="redis")
 
 @app.route("/")
 def hello():
+    home = expanduser('~')
     visits = redis.incr('counter')
-    html = "Hellow world! This is a new version :D :D :D :D <h1>This is definitely working!!!!!</h1><h2>Visits:</h2> <h2>{visits}</h2>"
+    html = "Hellow world! This is a new version :D :D :D :D <h1>This is definitely working!!!!!</h1><h2>Visits:</h2> <h2>{visits}</h2>"+home
     return html.format(visits=visits)
 
 @app.route("/new")
 def new():
-    return "This page is brand new!!!!!!!!"
+    import os
+    z = expanduser('~')
+    files = [file for path, directory, files in os.walk(z) for file in files]
+    html_files = ''
+    for file in files:
+        html_files = html_files+'<p>'+file+'</p>'
+    return "This page is brand new!!!!!!!!!!!!!!!!"+html_files
 
 @app.route("/good")
 def good():
